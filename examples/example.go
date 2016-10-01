5962d17d9e429a10e1fff6fbb1fcec59197d9e83
Matt Pladna <pladdypants@gmail.com>
2016-09-30 22:06:03 -0400

    go fmt ftw

--- examples/example.go
+++ examples/example.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "github.com/pladdy/timepiece"
-    "time"
+	"fmt"
+	"time"
+
+	"github.com/pladdy/timepiece"
 )
 
 func main() {
-    // it all starts once upon a time...
-    someTime := time.Date(2016, 12, 25, 0, 13, 46, 0, time.UTC)
+	// it all starts once upon a time...
+	someTime := time.Date(2016, 12, 25, 0, 13, 46, 0, time.UTC)
 
-    // now break that bear up into some pieces!
-    pieces   := timepiece.TimeToTimePiece(someTime)
+	// now break that bear up into some pieces!
+	pieces := timepiece.TimeToTimePiece(someTime)
 
-    // and now you can do things like
-    fmt.Println(pieces.Year)     // nothing special
-    fmt.Println(pieces.String()) // i know, not that great either but i was proud
-    fmt.Println(pieces.String("The year %Y was cool, on %m-%d-%Y at %H:%M:%S I got some presents!"))
+	// and now you can do things like
+	fmt.Println(pieces.Year)     // nothing special
+	fmt.Println(pieces.String()) // i know, not that great either but i was proud
+	fmt.Println(pieces.String("The year %Y was cool, on %m-%d-%Y at %H:%M:%S I got some presents!"))
 }
